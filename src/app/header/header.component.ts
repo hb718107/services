551fd26ee01bbf81f910ab0d66316e59da2412a1
hb718107 <hb718107@gmail.com>
2025-12-15 18:31:53 +0500

    initial commit..

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  selectedTab: string = 'home';
+
+  //When HOME Link is clicked
+  HomeClicked(){
+    this.selectedTab = 'home';
+  }
+
+  //When Admin Link is clicked
+  AdminClicked(){
+    this.selectedTab = 'admin';
+  }
+
+  OnSubscribe(){
+    alert('Subscribed Successfully!');
+  }
+}
